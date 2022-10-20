89995c8a02685f3fafc0553193a280b064ef75d2
pudimlucy <dourolucy@gmail.com>
2022-10-20 11:52:15 -0300

    --- Final Adjustments 1.6 --- 1. Aesthetic changes

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.page, name="title"),
     path("wiki/search/", views.get_search, name="search"),
-    path("/new", views.new, name="new"),
-    path("/edit", views.edit, name="edit"),
-    path("/random", views.random, name="random"),
+    path("new", views.new, name="new"),
+    path("edit", views.edit, name="edit"),
+    path("random", views.random, name="random"),
 ]
