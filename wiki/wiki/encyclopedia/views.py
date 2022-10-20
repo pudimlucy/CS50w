9c70b01ef2c81a0c9a09165bef8f9437ff51b125
pudimlucy <dourolucy@gmail.com>
2022-10-20 10:49:26 -0300

    pls

--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -10,10 +10,11 @@ markdowner = Markdown()
 
 def index(request):
 
+    entries = util.list_entries()
     return render(
         request,
         "encyclopedia/index.html",
-        {"entries": util.list_entries(), "form": form},
+        {"entries": entries, "form": form},
     )
 
 
@@ -108,13 +109,12 @@ def new(request):
 
 def edit(request):
     title = request.POST.get("edit")
-    eform = forms.editpageform(initial={"title": title, "body": util.get_entry(title)})
+    eform = forms.editpageform(initial={'title':title, 'body':util.get_entry(title)})
 
-    return render(
-        request,
-        "encyclopedia/edit_page.html",
-        {"title": title, "form": form, "eform": eform},
-    )
+    return render(request, "encyclopedia/edit_page.html",{
+            "title": title,
+            "form":form,
+            "eform":eform})
 
 
 def save(request):
@@ -123,11 +123,12 @@ def save(request):
     if eform.is_valid():
         title = eform.cleaned_data["title"]
         content = eform.cleaned_data["body"]
-
+        
         util.save_entry(title, content)
         return page(request, title)
 
     else:
-        return render(
-            request, "encyclopedia/edit_page.html", {"form": form, "eform": eform}
-        )
+        return render (request, "encyclopedia/edit_page.html",{
+                "form":form,
+                "eform":eform            
+        })
