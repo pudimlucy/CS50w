88553d25fec9dad51797ee6711c6ebe5a078c49c
pudimlucy <dourolucy@gmail.com>
2022-10-20 10:42:12 -0300

    --- Edit Page 3.4.1 --- Bug fix

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("wiki/<str:title>", views.page, name="title"),
     path("/new", views.new, name="new"),
     path("/edit", views.edit, name="edit"),
-    path("save/", views.save, name="save"),
-    path("wiki/<str:title>", views.page, name="title"),
+    path("save/", views.save, name="save"), 
     path("wiki/search/", views.get_search, name="search"),
 ]
