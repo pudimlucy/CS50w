59c8b9b584db7b9e7091a7e8ea5f28dd0fa9a94e
pudimlucy <dourolucy@gmail.com>
2023-05-02 11:27:18 -0300

    --- Mail 1.0 --- Implemented:     1. Send email     2. Mailbox     3. View email

--- mail/mail/mail/static/mail/inbox.js
+++ mail/mail/mail/static/mail/inbox.js
@@ -4,10 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  // TODO: event listener for clicking on email
-  // TODO: event listener for clicking on archive/unarchive button
-  // TODO: event listener for clicking on reply button
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit', send_email);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -16,8 +14,8 @@ document.addEventListener('DOMContentLoaded', function() {
 function compose_email() {
 
   // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#email-view').style.display = 'none';
 
   // Clear out composition fields
@@ -36,27 +34,87 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  // TODO: load appropriate mailbox's content
-  // GET request to /emails/<mailbox>
-  // display emails info in emails-view separating by divs
-  // read -> gray background, unread -> white background
+  // Load  mailbox's content
+  fetch('/emails/' + mailbox)
+  .then(response => response.json())
+  .then(emails => {
+    emails.forEach(email => {
+      let div = document.createElement('div');
+      div.style.border = 'thin solid black';
+      if (email['read']) {
+        div.style.backgroundColor = 'grey'
+      }
+      div.innerHTML = `
+        <ul>
+          <li><b>From: ${email['sender']}</b></li>
+          <li><b>Subject: ${email['subject']}</b></li>
+          <li><b>Time: ${email['timestamp']}</b></li>
+        </ul>
+      `;
+      // Adds event listener to display email
+      div.addEventListener('click', () => view_email(email['id']))
+      document.querySelector('#emails-view').append(div);
+    });
+  });
 }
 
-function send_email() {
-  // TODO: sends email
-  // get user's composition form
-  // POST request to /emails
+function send_email(event) {
+  // Prevents from displaying inbox
+  event.preventDefault()
 
-  // load user's sent mailbox
-  load_mailbox('sent')
+  // Creates email with form's contents
+  fetch('/emails' , {
+    method: 'POST',
+    body: JSON.stringify
+    ({
+      recipients: document.querySelector('#compose-recipients').value,
+      subject: document.querySelector('#compose-subject').value,
+      body: document.querySelector('#compose-body').value
+    })
+  })
+
+  // Displays sent mailbox
+  .then(response => response.json())
+  .then(response => {
+    load_mailbox('sent');
+  });
 }
 
-function view_email() {
-  // TODO: display content of email when clicked
-  // GET request to /emails/<email_id>
-  // display sender, recipients, subject, timestamp and body on email-view
-  // hide emails-view and compose-view, show email-view 
-  // PUT request to /emails/<email_id> to update read property
+function view_email(id) {
+
+  // Shows email view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'block';
+
+  // Fetches email
+  fetch('/emails/' + id)
+  .then(response => response.json())
+  .then(email => {
+
+    // Displays email's content
+    document.querySelector('#email-view').innerHTML = `
+    <ul>
+      <li><b>From: ${email['sender']}</b></li>
+      <li><b>To: ${email['recipients']}</b></li>
+      <li><b>Subject: ${email['subject']}</b></li>
+      <li><b>Time: ${email['timestamp']}</b></li>
+    </ul>
+    <p class="m-2">${email['body']}</p>
+    `
+
+    // Marks email as read if not read
+    if (!email['read']) {
+      fetch('/emails/' + email['id'], {
+        method: 'PUT',
+        body: JSON.stringify({
+            read: true
+        })
+      })      
+    }
+  })
+
+  // TODO: create archive, unarchive, reply buttons
 }
 
 function archive () {
@@ -65,7 +123,7 @@ function archive () {
   // PUT request to /emails/<email_id> to update archived property accordingly
 
   // loads user's inbox
-  load_mailbox('inbox')
+  load_mailbox('inbox');
 }
 
 function reply () {
